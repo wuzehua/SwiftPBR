3501582049a5c86e263a7150e7218e6c421fea5a
RainZhong <1322030678@qq.com>
2020-01-13 01:57:18 +0800

    add inverse for matrix

--- Sources/SwiftPBR/main.swift
+++ Sources/SwiftPBR/main.swift
@@ -1,3 +1,3 @@
 print("Hello, world!")
-let v = Vector2f(x: 1.0, y: 1.0)
-print(v.length)
+let m = inverse(m: Matrix(scale: 2))
+print(m.str)
--- Sources/SwiftPBR/math/matrix.swift
+++ Sources/SwiftPBR/math/matrix.swift
@@ -7,6 +7,22 @@ import Foundation
 struct Matrix{
     var data:[DataType]
 
+    var str: String{
+        get{
+            var ret = ""
+            for i in 0..<4{
+                ret += "|"
+                for j in 0..<4{
+                    ret += "\(data[4 * i + j]) "
+                }
+
+                ret += "|\n"
+            }
+
+            return ret
+        }
+    }
+
     init(scale: DataType = 1){
         data = [DataType]()
         for i in 0...3{
@@ -69,6 +85,8 @@ struct Matrix{
         }
     }
 
+
+
 }
 
 func transpose(m: Matrix) -> Matrix{
@@ -83,5 +101,74 @@ func transpose(m: Matrix) -> Matrix{
 }
 
 func inverse(m: Matrix) -> Matrix{
+    var U = Matrix(scale: 0)
+    var L = Matrix(scale: 1)
+    var L_inverse = L
+    var U_inverse = U
+    var ret = U
 
+    var sum: DataType = 0
+
+    for i in 0..<4{
+        for j in i..<4{
+            sum = 0
+            for k in 0..<i{
+                sum += (L[j,k] * U[k,j])
+            }
+
+            U[i,j] = m[i,j] - sum
+        }
+
+        for j in (i + 1)..<4{
+            sum = 0
+            for k in 0..<i{
+                sum += (L[j,k] * U[k,i])
+            }
+
+            if(U[i,i] == 0){
+                return ret
+            }
+
+            L[j,i] = (m[j,i] - sum) / U[i,i]
+        }
+    }
+
+    for i in 1..<4{
+        for j in 0..<i{
+            sum = 0
+            for k in 0..<i{
+                sum += (L[i,k] * L_inverse[k,j])
+            }
+
+            L_inverse[i,j] = -sum
+        }
+    }
+
+    for i in 0..<4{
+        U_inverse[i,i] = 1 / U[i,i]
+    }
+
+
+
+    for i in 0..<4{
+
+        for j in (0..<i).reversed(){
+            sum = 0
+            for k in (j + 1)...i{
+                sum += (U[j,k] * U_inverse[k,i])
+            }
+
+            U_inverse[j,i] = -sum / U[j,j]
+        }
+    }
+
+    for i in 0..<4{
+        for j in 0..<4{
+            for k in 0..<4{
+                ret[i,j] += (U_inverse[i,k] * L_inverse[k,j])
+            }
+        }
+    }
+
+    return ret
 }
